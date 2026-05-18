4af4111510da828c784bc62ad920193aae1a6173
Bambosz13 <bartek.wykrota@gmail.com>
2026-05-18 20:52:42 +0200

    Remove unnecessary comment in ChartData class
    Removed comment about data analysis variable.

--- ChartData.cs
+++ ChartData.cs
@@ -5,7 +5,6 @@ namespace MvcProject.Models
         public string[] Labels { get; set; }
         public int[] Values { get; set; }
         
-        // Zmienna przechowująca wynik naszej "analizy danych" po stronie serwera
         public double AverageValue { get; set; } 
     }
 }
